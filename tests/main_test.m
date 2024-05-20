8ec99c2f8968ffc4fb7bcabb4f9a139f39f562c7
Natee_Panagant <natepa@kku.ac.th>
2024-05-20 22:38:25 +0700

    revise testing script to show the error summary

--- tests/main_test.m
+++ tests/main_test.m
@@ -1,9 +1,12 @@
 clear all;close all;clc;
 %Add main path
-current_dir = pwd;
+file_dir = mfilename('fullpath');
+sidx = strfind(file_dir,'\');
+current_dir = file_dir(1:sidx(end));
+main_dir = file_dir(1:sidx(end-1));
+
 cd(current_dir);
-sidx = strfind(current_dir,'\');
-main_dir = current_dir(1:sidx(end)-1);
+
 addpath(genpath(main_dir));
 format long g
 
@@ -23,6 +26,8 @@ flag_p1 = 0;%Plot Ajj
 flag_p2 = 0;%Plot Qjj
 flag_p3 = 0;%Plot Cp
 flag_p4 = 0;%Plot VLM Loadings
+
+err_idx = zeros(numel(clist),1);
 for ci = csel
     cname = clist{ci};
     fpath = [pwd '\PanelAero_Validation_RST\' cname '\'];
@@ -68,14 +73,13 @@ for ci = csel
     disp(' ');
     disp(repmat('-',1,60));
 
-    %%%%%%%%%%%%%%%%%%
-    % VLM Validation %
-    %%%%%%%%%%%%%%%%%%
+    %%%%%%%
+    % VLM %
+    %%%%%%%
     disp(' ');
     disp('VLM testing - Current vs Ref_data');
     disp(' ');
 
-    % Current version of VLM code
     [D0,A,GAMMA,RHS,qxV,qyV,qzV,F_VLM]=VLM(M,Q,rho_air,Sc,Sm,Si,So,S,pspan,normvec);
     wj = (Q(1)*normvec(:,1)+Q(2)*normvec(:,2)+Q(3)*normvec(:,3))/Qinfabs;
 
@@ -83,23 +87,14 @@ for ci = csel
     Qjj_VLM = -inv(Ajj_VLM);
     Cp_VLM = Qjj_VLM*wj;
 
-    % Load reference validated results
-    load(['rst_' clist{ci} '.mat']);
-
-    % Comparison
-    disp(['Maximum absolute diff of Cp = ' num2str(max(max(max(abs(Cp_VLM_ref-Cp_VLM)))))]);
-    disp(['Maximum absolute diff of Qjj (AIC) = ' num2str(max(max(max(abs(Qjj_VLM_ref-Qjj_VLM)))))]);
-    disp(' ');
-
-    %%%%%%%%%%%%%%%%%%
-    % DLM Validation %
-    %%%%%%%%%%%%%%%%%%
+    %%%%%%%
+    % DLM %
+    %%%%%%%
     disp(repmat('-',1,60));
     disp(' ');
     disp('DLM testing - Current vs Ref_data');
     disp(' ');
 
-    % Current code execution
     D=DLM(Sc,Si,Sm,So,M,k,normvec,pspan,pchord,D0);
     wj = (Q(1)*normvec(:,1)+Q(2)*normvec(:,2)+Q(3)*normvec(:,3))/Qinfabs;
 
@@ -111,22 +106,65 @@ for ci = csel
 
     Qjj_DLM3 = -inv(D{1,3});
     Cp_DLM3 = Qjj_DLM3*wj;
+    
+    % Load reference validated VLM+DLM results
+    load(['rst_' clist{ci} '.mat']);
 
-    % PanelAero (DLR) - load result only
-    disp(['%Max real diff of Cp1 (k = ' num2str(k(1)) ') = ' num2str(max(max(max(abs((real(Cp_DLM1_ref)-real(Cp_DLM1)))))))]);
-    disp(['%Max imag diff of Cp1 (k = ' num2str(k(1)) ') = ' num2str(max(max(max(abs((imag(Cp_DLM1_ref)-imag(Cp_DLM1)))))))]);
-    disp(['%Max real diff of Cp1 (k = ' num2str(k(2)) ') = ' num2str(max(max(max(abs((real(Cp_DLM2_ref)-real(Cp_DLM2)))))))]);
-    disp(['%Max imag diff of Cp1 (k = ' num2str(k(2)) ') = ' num2str(max(max(max(abs((imag(Cp_DLM2_ref)-imag(Cp_DLM2)))))))]);
-    disp(['%Max real diff of Cp1 (k = ' num2str(k(3)) ') = ' num2str(max(max(max(abs((real(Cp_DLM3_ref)-real(Cp_DLM3)))))))]);
-    disp(['%Max imag diff of Cp1 (k = ' num2str(k(3)) ') = ' num2str(max(max(max(abs((imag(Cp_DLM3_ref)-imag(Cp_DLM3)))))))]);
+    % Comparison
+    rst_diff(1) = max(max(max(abs(Cp_VLM_ref-Cp_VLM))));
+    rst_diff(2) = max(max(max(abs(Qjj_VLM_ref-Qjj_VLM))));
+
+    rst_diff(3) = max(max(max(abs((real(Cp_DLM1_ref)-real(Cp_DLM1))))));
+    rst_diff(4) = max(max(max(abs((imag(Cp_DLM1_ref)-imag(Cp_DLM1))))));
+    rst_diff(5) = max(max(max(abs((real(Cp_DLM2_ref)-real(Cp_DLM2))))));
+    rst_diff(6) = max(max(max(abs((imag(Cp_DLM2_ref)-imag(Cp_DLM2))))));
+    rst_diff(7) = max(max(max(abs((real(Cp_DLM3_ref)-real(Cp_DLM3))))));
+    rst_diff(8) = max(max(max(abs((imag(Cp_DLM3_ref)-imag(Cp_DLM3))))));
+
+    rst_diff(9) = max(max(max(abs((real(Qjj_DLM1_ref)-real(Qjj_DLM1))))));
+    rst_diff(10) = max(max(max(abs((imag(Qjj_DLM1_ref)-imag(Qjj_DLM1))))));
+    rst_diff(11) = max(max(max(abs((real(Qjj_DLM2_ref)-real(Qjj_DLM2))))));
+    rst_diff(12) = max(max(max(abs((imag(Qjj_DLM2_ref)-imag(Qjj_DLM2))))));
+    rst_diff(13) = max(max(max(abs((real(Qjj_DLM3_ref)-real(Qjj_DLM3))))));
+    rst_diff(14) = max(max(max(abs((imag(Qjj_DLM3_ref)-imag(Qjj_DLM3))))));
+
+    disp(['Maximum absolute diff of Cp = ' num2str(rst_diff(1))]);
+    disp(['Maximum absolute diff of Qjj (AIC) = ' num2str(rst_diff(2))]);
     disp(' ');
-    disp(['%Max real diff of Qjj1 (k = ' num2str(k(1)) ') = ' num2str(max(max(max(abs((real(Qjj_DLM1_ref)-real(Qjj_DLM1)))))))]);
-    disp(['%Max imag diff of Qjj1 (k = ' num2str(k(1)) ') = ' num2str(max(max(max(abs((imag(Qjj_DLM1_ref)-imag(Qjj_DLM1)))))))]);
-    disp(['%Max real diff of Qjj2 (k = ' num2str(k(2)) ') = ' num2str(max(max(max(abs((real(Qjj_DLM2_ref)-real(Qjj_DLM2)))))))]);
-    disp(['%Max imag diff of Qjj2 (k = ' num2str(k(2)) ') = ' num2str(max(max(max(abs((imag(Qjj_DLM2_ref)-imag(Qjj_DLM2)))))))]);
-    disp(['%Max real diff of Qjj3 (k = ' num2str(k(3)) ') = ' num2str(max(max(max(abs((real(Qjj_DLM3_ref)-real(Qjj_DLM3)))))))]);
-    disp(['%Max imag diff of Qjj3 (k = ' num2str(k(3)) ') = ' num2str(max(max(max(abs((imag(Qjj_DLM3_ref)-imag(Qjj_DLM3)))))))]);
+    disp(['%Max real diff of Cp1 (k = ' num2str(k(1)) ') = ' num2str(rst_diff(3))]);
+    disp(['%Max imag diff of Cp1 (k = ' num2str(k(1)) ') = ' num2str(rst_diff(4))]);
+    disp(['%Max real diff of Cp1 (k = ' num2str(k(2)) ') = ' num2str(rst_diff(5))]);
+    disp(['%Max imag diff of Cp1 (k = ' num2str(k(2)) ') = ' num2str(rst_diff(6))]);
+    disp(['%Max real diff of Cp1 (k = ' num2str(k(3)) ') = ' num2str(rst_diff(7))]);
+    disp(['%Max imag diff of Cp1 (k = ' num2str(k(3)) ') = ' num2str(rst_diff(8))]);
     disp(' ');
+    disp(['%Max real diff of Qjj1 (k = ' num2str(k(1)) ') = ' num2str(rst_diff(9))]);
+    disp(['%Max imag diff of Qjj1 (k = ' num2str(k(1)) ') = ' num2str(rst_diff(10))]);
+    disp(['%Max real diff of Qjj2 (k = ' num2str(k(2)) ') = ' num2str(rst_diff(11))]);
+    disp(['%Max imag diff of Qjj2 (k = ' num2str(k(2)) ') = ' num2str(rst_diff(12))]);
+    disp(['%Max real diff of Qjj3 (k = ' num2str(k(3)) ') = ' num2str(rst_diff(13))]);
+    disp(['%Max imag diff of Qjj3 (k = ' num2str(k(3)) ') = ' num2str(rst_diff(14))]);
+    disp(' ');
+
+    if max(rst_diff) > 1e-10
+        err_idx(ci) = 1;
+    end
 end
 disp(repmat('%',1,60));
-    
+disp(' ');
+disp(repmat('*',1,60));
+disp(repmat('*',1,60));
+disp(repmat('*',1,60));
+if sum(err_idx)>0
+    disp(['found error in following cases:']);
+    for ci=1:numel(err_idx)
+        if err_idx(ci)>0
+            disp(clist{ci});
+        end
+    end
+else
+    disp(['All results converged, no errors found']);
+end
+disp(repmat('*',1,60));
+disp(repmat('*',1,60));
+disp(repmat('*',1,60));
